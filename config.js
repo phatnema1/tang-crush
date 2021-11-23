18e58fe6f25d1040359778c723c6772dfbd15be1
phatnema1 <94908739+phatnema1@users.noreply.github.com>
2021-11-23 20:01:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'haaluu :3!',
+    introDesc: `chúc sinh nhật vui vẻ nhaa :33`,
+    btnIntro: 'Ỏ',
+    title: 'khum có văn thơ dì nhìu nên mong m bớt cọc hay bùn và hiểu t hơn ',
+    desc: 'đôi lúc t cx bận mà thấy m cọc t sợ á :((( bớt nhaa ',
+    btnYes: 'ò uuki',
+    btnNo: 'đéo :)))',
+    question:'thế gian vốn dĩ lạ thường . sao m đi nhầm đường chọn t ',
+    btnReply: 'Gửi cho tao :>',
+    reply: 'thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích thích <33333333',
+    mess: 'ò bít mà fak you :))) ',
+    messDesc: 'chúc mài có 1 ngày vui vẻ nhó :>>',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/e.bo.a.roi
 }
